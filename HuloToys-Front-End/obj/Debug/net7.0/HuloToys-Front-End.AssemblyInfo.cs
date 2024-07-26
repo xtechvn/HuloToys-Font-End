8de0ebfd8070aec92132922028f6ab02244936ac
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2024-07-26 17:36:03 +0700

    Add HTML to FE
    Add HTML to FE

--- HuloToys-Front-End/obj/Debug/net7.0/HuloToys-Front-End.AssemblyInfo.cs
+++ HuloToys-Front-End/obj/Debug/net7.0/HuloToys-Front-End.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HuloToys-Front-End")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5072ac81af52557822fec64138fdfa34d99cfc29")]
 [assembly: System.Reflection.AssemblyProductAttribute("HuloToys-Front-End")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HuloToys-Front-End")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
