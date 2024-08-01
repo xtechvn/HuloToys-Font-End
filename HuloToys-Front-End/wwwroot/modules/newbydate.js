906d34c91ab95a0a0dc697fcbb88802dae72887a
Hiếu <anhhieuk50@gmail.com>
2024-08-01 22:23:35 +0700

    update js

--- HuloToys-Front-End/wwwroot/modules/newbydate.js
+++ HuloToys-Front-End/wwwroot/modules/newbydate.js
@@ -1,9 +1,9 @@
-﻿
-var _new_html = '<a href="{link}">{title}</a>';
-$(document).ready(function () {
+﻿$(document).ready(function () {
     _load_new_date.loadnew()
 
 })
+var _new_html = '<a href="{link}">{title}</a>';
+
 var _load_new_date = {
     loadnew: function () {
         $.ajax({
