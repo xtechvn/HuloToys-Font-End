e56a5f0dc0655e2ade95d6fe0129a3547d263c9e
Hiếu <anhhieuk50@gmail.com>
2024-08-01 22:48:43 +0700

    update tin

---
+++ HuloToys-Front-End/wwwroot/modules/newdetail.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function() {
+    _new.Initialization()
+
+})
+var _newdetail = {
+    Initialization: function () {
+        _newdetail.getNewsMostViewedArticle(1, 10, 1);
+
+    },
+    getNewsMostViewedArticle: function (page, size, category_id) {
+        var requestObj = {
+            skip: page,
+            take: size,
+            category_id: category_id
+        };
+        $.ajax({
+            url: "/News/NewsMostViewedArticle",
+            type: 'post',
+            data: { requestObj: requestObj },
+            success: function (data) {
+
+                $("#Most_Viewed_Article").html(data);
+            },
+
+        });
+    },
+    l
+}
