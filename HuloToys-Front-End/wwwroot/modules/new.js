76641d613ea2e46cdd38a74c659a9d8fdddd8c77
Hiếu <anhhieuk50@gmail.com>
2024-08-01 14:44:59 +0700

    update tin tuc

--- HuloToys-Front-End/wwwroot/modules/new.js
+++ HuloToys-Front-End/wwwroot/modules/new.js
@@ -9,9 +9,7 @@ var _new = {
         _new.getNewsByTag(1, 10, 1);
         _new.getNewsPinned(1, 10, 1);
         _new.getNewsMostViewedArticle(1, 10, 1);
-        _new.getNewsBySale(1, 10, 1);
-
-
+        
     },
  
  
