02bcad4b07c3c816dbc5076e81950b714a65e86e
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2024-09-26 18:15:19 +0700

    Update ProductInsertRaitingRequestModel.cs

--- HuloToys-Front-End/Models/Raiting/ProductInsertRaitingRequestModel.cs
+++ HuloToys-Front-End/Models/Raiting/ProductInsertRaitingRequestModel.cs
@@ -9,7 +9,7 @@ namespace HuloToys_Front_End.Models.Raiting
     public class ProductInsertRaitingRequestModel
     {
         public long order_id { get; set; }
-        public long product_id { get; set; }
+        public string product_id { get; set; }
         public string token { get; set; }
         public float star { get; set; }
         public string comment { get; set; }
