b08353d92e774ab70cdd29093920ad7fe69fa52b
Đăng Tây <nguyendang01092003@gmail.com>
2025-04-21 10:35:29 +0700

    Up2

--- HuloToys-Front-End/wwwroot/modules/new.js
+++ HuloToys-Front-End/wwwroot/modules/new.js
@@ -1,11 +1,8 @@
 ﻿$(document).ready(function () {
     _new.Initialization();
     _home_product.Initialization();
-    $("#text_input").on('keyup', function (e) {
-        if (e.key === 'Enter' || e.keyCode === 13) {
-            _new.GetFindArticleByTitle();
-        }
-    });
+
+   
     var category_id2 = parseInt($(".category_id").data("categoryid"));
 
     const query_string = window.location.search;
@@ -17,32 +14,14 @@
     _new.bin_news_home(category_id2, page);
 
 
-    //if (category_id <= 0) {
-    //    // Bin theo tin mới nhất của các chuyên mục
-    //    news.bin_news_top(category_id, "top_story", page);
-    //    news.bin_news_left(category_id, "top_left", page);
-    //} else {
-    //    news.bin_news_top(category_id, "category_top", page);
-    //    news.bin_news_left(category_id, "category_left", page);
-    //}    
+
 
 })
 let category_id = 22;
 let category_id_menu = 22;
 var _new = {
     Initialization: function () {
-        var categoryIdSession = sessionStorage.getItem('NewsCategoryId');
-        if (categoryIdSession) {
-            _new.getNewsByTag(1, 10, categoryIdSession);
-            /*_new.getNewsPinned1(1, 1, categoryIdSession);*/
 
-        } else {
-            _new.getNewsByTag(1, 10, category_id_menu);
-            /*_new.getNewsPinned1(1, 1, 10);*/
-
-        }
-
-        _new.NewsCategory(category_id_menu);
         _new.getNewsMostViewedArticle(1, 3, category_id_menu);
 
     },
@@ -61,75 +40,6 @@ var _new = {
             }
         });
 
-    },
-
-
-
-    NewsCategory: function (category_id) {
-        var requestObj = {
-            category_id: category_id,
-        };
-        $.ajax({
-            url: "/News/NewsCategory",
-            type: 'post',
-            data: { requestObj: requestObj },
-            success: function (data) {
-                $("#news-category").html(data);
-            },
-
-        });
-    },
-    getNewsPinned1: function (page, size, category_id) {
-        $('#article-1').show();
-        var requestObj = {
-            skip: page,
-            take: size,
-            category_id: category_id,
-            Pinned: 1
-        };
-        $.ajax({
-            url: "/News/NewsPinned",
-            type: 'post',
-            data: { requestObj: requestObj },
-            success: function (data) {
-
-                $("#article").html(data);
-
-            },
-
-        });
-    },
-    getNewsByTag: function (page, size, category_id, page_url) {
-        $('#article-1').show();
-        $('#article-2').show();
-        $('#article-3').show();
-        _new.getNewsPinned1(page == 1 ? 0 : page, size, category_id);
-        category_id = category_id;
-
-        var requestObj = {
-            skip: page,
-            take: size,
-            category_id: category_id
-        };
-        sessionStorage.setItem('NewsCategoryId', Number(category_id));
-        var id = parseFloat(category_id);
-        $.ajax({
-            url: "/News/NewsByTag",
-            type: 'post',
-            data: { requestObj: requestObj },
-            success: function (data) {
-
-                $("#section-article-paginate").html(data);
-                $('.list-news-top').show();
-                $('.cat-tag').removeClass('active');
-                $('.tag_' + id).addClass('active');
-                $(".paging_" + page).addClass("active")
-                if (page_url != null && page_url != "")
-                    window.history.pushState('string', '', page_url)
-                $(window).scrollTop(0);
-            },
-
-        });
     },
 
     getNewsMostViewedArticle: function (page, size, category_id) {
@@ -165,53 +75,29 @@ var _new = {
 
         });
     },
-    paging: function (page) {
-        var categoryIdSession = sessionStorage.getItem('NewsCategoryId');
-        _new.getNewsByTag(page, 10, parseFloat(categoryIdSession))
-        _new.getNewsPinned1(page, 10, parseFloat(categoryIdSession));
-    },
-    prev: function (min) {
-        var page = $('.pagination .active').attr('data-page');
-        if (page != min) {
-            page = parseFloat(page) - 1;
-            var categoryIdSession = sessionStorage.getItem('NewsCategoryId');
-            _new.getNewsByTag(page, 10, parseFloat(categoryIdSession))
-            _new.getNewsPinned1(page, 10, parseFloat(categoryIdSession));
-        }
+    //GetFindArticleByTitle: function () {
+    //    $('#article-1').hide();
+    //    $('#article-2').hide();
+    //    $('#article-3').hide();
+    //    $('.list-news-top').hide();
+    //    $('#section-article-paginate').hide();
+    //    var requestObj = {
+    //        title: $('#text_input').val(),
+    //        parent_cate_faq_id: category_id
+    //    };
+    //    $.ajax({
+    //        url: "/News/GetFindArticleByTitle",
+    //        type: 'post',
+    //        data: { requestObj: requestObj },
+    //        success: function (data) {
+    //            $('#section-article-paginate').show();
+    //            $("#section-article-paginate").html(data);
 
-    },
-    next: function (max) {
-        var page = $('.pagination .active').attr('data-page');
-        if (page != max) {
-            page = parseFloat(page) + 1;
-            var categoryIdSession = sessionStorage.getItem('NewsCategoryId');
-            _new.getNewsByTag(page, 10, parseFloat(categoryIdSession))
-            _new.getNewsPinned1(page, 10, parseFloat(categoryIdSession));
-        }
+    //        },
 
-    },
-    GetFindArticleByTitle: function () {
-        $('#article-1').hide();
-        $('#article-2').hide();
-        $('#article-3').hide();
-        $('.list-news-top').hide();
-        $('#section-article-paginate').hide();
-        var requestObj = {
-            title: $('#text_input').val(),
-            parent_cate_faq_id: category_id
-        };
-        $.ajax({
-            url: "/News/GetFindArticleByTitle",
-            type: 'post',
-            data: { requestObj: requestObj },
-            success: function (data) {
-                $('#section-article-paginate').show();
-                $("#section-article-paginate").html(data);
-
-            },
-
-        });
-    },
+    //    });
+    //},
+   
 }
 var _home_product = {
     Initialization: function () {
