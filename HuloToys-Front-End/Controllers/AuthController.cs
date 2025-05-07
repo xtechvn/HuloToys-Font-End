dadb33744fdd23d3a800f7447eb653e69f0e7154
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-05-07 19:13:39 +0700

    fix sign-in with google

--- HuloToys-Front-End/Controllers/AuthController.cs
+++ HuloToys-Front-End/Controllers/AuthController.cs
@@ -34,6 +34,7 @@ public class AuthController : Controller
             string clientId = _configuration["Authentication:Google:ClientId"];
             string clientSecret = _configuration["Authentication:Google:ClientSecret"];
             string redirectUri = _configuration["Authentication:Google:RedirectUri"];
+            HttpContext.Response.Headers.Add("Cross-Origin-Opener-Policy", "same-origin");
 
             var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
             {
--- HuloToys-Front-End/wwwroot/modules/account_google.js
+++ HuloToys-Front-End/wwwroot/modules/account_google.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
-    var googleClientId = '65575993345-haqgehmq7lr2spseivas34237t3qkuos.apps.googleusercontent.com'; // Thay YOUR_GOOGLE_CLIENT_ID bằng Client ID của bạn
-    var redirectUri = 'https://localhost:2944/api/Auth/GoogleSignInCallback'
+    var googleClientId = '231824741925-dr41odfkd6jm8l2oochnt0efvn842t4e.apps.googleusercontent.com'; // Thay YOUR_GOOGLE_CLIENT_ID bằng Client ID của bạn
+    var redirectUri = 'http://qc-fe-hulotoy.x-tech.vn/api/Auth/GoogleSignInCallback'
     //google.accounts.id.initialize({
     //    client_id: googleClientId,
     //    callback: handleCredentialResponse
@@ -56,22 +56,26 @@
     //}
     $('.btn-login-gg').on('click', function () {
         // Xác định URL ủy quyền của Google
-        const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=' + googleClientId + '&redirect_uri=' + redirectUri +'&response_type=code&scope=openid%20profile%20email&prompt=select_account';
+        const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=' + googleClientId + '&redirect_uri=' + redirectUri + '&response_type=code&scope=openid%20profile%20email&prompt=select_account';
 
         // Mở popup
         const popupWindow = window.open(googleAuthUrl, 'google_login', 'width=600,height=500');
-
         // Theo dõi cửa sổ popup và xử lý khi nó đóng
         const popupInterval = setInterval(() => {
             if (popupWindow && popupWindow.closed) {
                 clearInterval(popupInterval);
                 // Kiểm tra xem dữ liệu đã được truyền từ popup chưa
-                if (localStorage.getItem(STORAGE_NAME.Login) || sessionStorage.getItem(STORAGE_NAME.Login)) {
+                var token_local = localStorage.getItem(STORAGE_NAME.Login);
+                if (token_local == null || token_local == undefined || token_local.trim() == '' || token_local.trim() == 'null'
+                    || token_local.trim() == 'undefined') {
+                    $(':input[type="submit"]').prop('disabled', false);
+
+                    $('#dangnhap .user input').closest('.form-group').find('.err').show()
+                    $('#dangnhap .user input').closest('.form-group').find('.err').html(NOTIFICATION_MESSAGE.LoginIncorrect)
+                } else {
+                   
                     window.location.reload();
 
-                } else {
-                    console.log('Popup đăng nhập Google đã đóng mà không có dữ liệu.');
-                    // Xử lý trường hợp người dùng đóng popup mà không đăng nhập
                 }
             }
         }, 500);
